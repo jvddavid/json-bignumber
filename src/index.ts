01cc3d5a09be814db78989ac5309931d31a7fac1
João Victor <j.victordavid2@gmail.com>
2024-07-08 17:25:03 -0300

    chore: add export type of BigNumber

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { JSONParse, type Options } from './lib/parse'
 import { stringify, type Replacer } from './lib/stringify'
 
+// BigNumber is a class, so we need to import it as a type
+import type BigNumber from 'bignumber.js'
+export type { BigNumber }
+
 const parse = JSONParse()
 
 // create the default method members with no options applied for backwards compatibility
